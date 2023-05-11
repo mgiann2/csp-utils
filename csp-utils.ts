28495cfadeb456f18958634497b298903b907d2a
Matteo Giannone <matteo.giannone@mail.utoronto.ca>
2023-05-11 15:01:59 -0400

    Added assignment methods to variable class

--- csp-utils.ts
+++ csp-utils.ts
@@ -11,6 +11,39 @@ export class Variable
         this.domain = [...domain]
         this.curr_domain = new Array(domain.length).fill(true)
     }
+
+    isAssigned(): boolean
+    {
+        return this.assigned_val !== null
+    }
+
+    assignValue(value: string): void 
+    {
+        if (this.isAssigned())
+        {
+            console.log("ERROR: Variable " + this.name + " is already assigned.")
+        } 
+        else if (this.domain.includes(value)) 
+        {
+            this.assigned_val = value
+        }
+        else
+        {
+            console.log("ERROR: value is not in variable's domain")
+        }
+    }
+
+    unassignValue(): void
+    {
+        if (this.isAssigned())
+        {
+            this.assigned_val = null
+        }
+        else
+        {
+            console.log("ERROR: Variable has not been assigned")
+        }
+    }
 }
 
 export class Constraint 
