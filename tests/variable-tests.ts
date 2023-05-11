b2ee415249786744bf6f91faf980a95ac4d0b4ad
Matteo Giannone <matteogiannone24@gmail.com>
2023-05-11 15:49:45 -0400

    Added test for Variable.isAssigned

--- tests/variable-tests.ts
+++ tests/variable-tests.ts
@@ -2,7 +2,8 @@ import { runTests } from "./test-helper.js"
 import { Variable } from "../csp-utils.js"
 
 const tests = [
-    testVariableCreation
+    testVariableCreation,
+    testIsAssigned
 ]
 
 function testVariableCreation(): boolean
@@ -14,6 +15,17 @@ function testVariableCreation(): boolean
     if (v.domain.toString() !== domain.toString()) {return false}
     if (v.curr_domain.toString() !== new Array(domain.length).fill(true).toString()) {return false}
     if (v.assigned_val !== null) {return false}
+    return true
+}
+
+function testIsAssigned(): boolean
+{
+    let name = "Var 1"
+    let domain = ['1', '2', '3']
+    let v = new Variable(name, domain)
+    if (v.isAssigned()) {return false}
+    v.assigned_val = '1'
+    if (!v.isAssigned()) {return false}
     return true
 }
 
